2409f251a22649c21bd22fbe9f9735494b588fbb
gabrielpcardozo <gabriel.cdz@hotmail.com>
2024-07-28 14:19:10 -0300

    Tentativa de autenticacao
    Nao consegui concluir os tipos de autenticacao como descrito na
    docuemntacao, a autenticacao e retornada no campo de URL do navegador,
    nao consegui fazer de uma forma para isso dar certo, vou subir ate
    sabendo que tenho um bug para resolver, Mas toda a parte de autenticacao
    esta construida acredito que seja uma questao de logica um alguma ponta solta.

--- SpotifyAPI/auth.rb
+++ SpotifyAPI/auth.rb
@@ -1,19 +1,24 @@
+#GEMs
 require 'sinatra'
 require 'rest-client'
 require 'securerandom'
 require 'uri'
 require 'json'
 
+#Criação da classe
 class SpotifyAuth
-  CLIENT_ID = 'bbbf86d4fca54a26981fff30fcf6b305'
-  CLIENT_SECRET = '669ee7038f2f421da2c506a6fbe48f41'
-  REDIRECT_URI = 'http://localhost:8888/callback'
+  #Declaração dos ID, aqui deve ser feito via arquivo .env
+  CLIENT_ID = 'a721dd02721d4accb5de00f929dbae48'
+  CLIENT_SECRET = '66cc77895a5747d387afec9129ac5cbe'
+  REDIRECT_URI = 'https://getaccess/callback'
   SCOPE = 'user-read-private user-read-email'
 
+  #Necessário a criação do state para previnir ataques CSRF.
   def self.generate_state
     SecureRandom.hex(8)
   end
 
+  #Contrução da URL de login e permissão de conta de usuário. 
   def self.login_url(state)
     query_params = {
       response_type: 'code',
@@ -24,8 +29,11 @@ class SpotifyAuth
     }
 
     "https://accounts.spotify.com/authorize?" + URI.encode_www_form(query_params)
+    #Exemplo da URL completa
+    #https://accounts.spotify.com/authorize?response_type=code&client_id=bbbf86d4fca54a26981fff30fcf6b305&scope=user-read-private user-read-email&redirect_uri=http://localhost:8888/callback&state=<16caracteres aleatorios>
   end
 
+  #trocar um código de autorização por um token de acesso
   def self.fetch_token(code)
     response = RestClient.post('https://accounts.spotify.com/api/token', {
       grant_type: 'authorization_code',
@@ -39,27 +47,64 @@ class SpotifyAuth
   end
 end
 
+#hAbilita as sessões do Servidor.
 enable :sessions
 
+#Cria a nossa url de Login para permitir que cada usuario APROVE a utilizacao dos dados.
 get '/login' do
   state = SpotifyAuth.generate_state
   session[:state] = state
+  logger.info("state: #{state}")
   redirect SpotifyAuth.login_url(state)
+  #<localhost>/login -> é o path correto para garantir os parametros.
+  #Apos a autorizacao
+    #https://getaccess/callback?code=AQD6-e_Kj7ltUvJz8T7yvoU-dk8rC6e-ZH_5FX4L2wtMJ86tQ1B1myFUj5E48ez7DUXqXx0t8nnI5QAdlc4073fwgsHVldIqq7YoekfX-flvLpUhdGB9bEzoRznhFLs-Li-BH4xmPCYTbAnoVXe7tNU5F2XtgkOr6EwncNV55bN7dPPHG_VAprxLMVXS_X-vZdJUwd0CkGEDShsFk7DHUNrz&state=441f6aa7fb707163
 end
 
+#Cria a url de <callback> para coelta do acces_token e informacoes necessarias como na Documentacao.
 get '/callback' do
   if params[:state] != session[:state]
+    logger.error("state Session #{session[:state]} \n state params #{params[:state]}")
     halt 400, "State mismatch"
   end
 
+  #Faz as trocas de dos codigos de autorizacao pelos token de acesso. 
   begin
     tokens = SpotifyAuth.fetch_token(params[:code])
+    logger.info("Acces_Token: #{tokens['access_token']}")
+    session[:access_token] = tokens['access_token']
     "Token de acesso: #{tokens['access_token']}"
   rescue RestClient::ExceptionWithResponse => e
+    logger.error("Failed to obtain token: #{e.response}")
     status e.http_code
     "Falha ao obter o token: #{e.response}"
   end
 end
 
-#code
-#AQCgTkO_TtxqCKqU4Qqs1F53dY8sSEdtPDxuya_zJwcpPvo0CNQR1uecItnfl_Hp4au-Ab2Bl4bEXDgT4o86Tw1LLXG9wr_hs3tm5lKyutxYi6d2sWqW11nUmAAGOCwvgborN6BaRu73CwhwOSjjmB7v0ezMOAueizmSAxrFDLwObu8Zw1-thfxMOTQ0-aMUm_oufk2x5cJeVDj0S-4HPWjMOWGExA&state=b30341f1604f5a79
+get '/profile' do
+  acces_token = session[:acces_token]
+
+  if acces_token.nil?
+    redirect '/login'
+  else 
+    #Chama a API
+    begin
+      user_info =RestClient.get('https://api.spotify.com/v1/me', {
+        Authorization: "Bearer #{access_token}"
+      })
+
+      #chama a api para coletas as informacoes do user
+      playlists = RestClient.get('https://api.spotify.com/v1/me/playlists', {
+        Authorization: "Bearer #{access_token}"
+      })
+
+      user_info_json =JSON.parse(user_info.body)
+      playlists_json =JSON.parse(playlists.body)
+
+      erb :profile, locals: { user_info: user_info_json, playlists: playlists_json }
+    rescue RestClient::ExceptionWithResponse => e
+      logger.error("Failed to fetch user info or playlists: #{e.response}")
+      "Erro ao obter informações do usuário ou playlists: #{e.response}"
+    end
+  end
+end
