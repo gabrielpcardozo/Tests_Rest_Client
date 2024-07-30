9d8997be315ac8f6f02ad2cfbd67697272780cba
gabrielpcardozo <gabriel.cdz@hotmail.com>
2024-07-29 21:06:40 -0300

    COleta dos dados da API
    Fiz algumas requisições na API e estou criando alguns tipos de
    formatações para coletar informações especificas.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+gem 'dotenv', groups: [:development, :test]
+
 gem 'rest-client', '~> 2.1'
 
 gem 'sinatra', '~> 3.1.0'
--- SpotifyAPI/services.rb
+++ SpotifyAPI/services.rb
@@ -1,30 +1,53 @@
 require 'rest-client'
 require 'json'
 #Metodo de autenticação temporário, enquanto não arrumo o arquivo de autenticação.
+require 'dotenv'
+Dotenv.load('teste.env')
 
-HEADERS = {Authorization: "Bearer BQBbd4mwboO5Z36eYkzbr44VSHAeRYVY2ayeTKjwURbMVD09qcGedzIz4CvkKdmiYHdOrbLWc7zDNsMxrOfzZYXNft6hG8Mz3vNilxRI7BfQ64ruI2H_c94SZ8_lD2stiW-45LgxQiBFkb4Z5-lIA01AQMTJCHZwqWANHXJnwRlMsaxpBDrxSAY59Fan5tYjaTA"}
-
-#profile_data = RestClient.get("https://api.spotify.com/v1/me", headers)
-
-#data = JSON.parse(profile_data.body)
-#puts data
 
 def get_profile(access_token)
   url = "https://api.spotify.com/v1/me"
-
-  response = RestClient.get(url,  Authorization: "Bearer #{access_token}")
-
-  return JSON.parse(response.body)
+  response = RestClient.get(url, { Authorization: "Bearer #{access_token}" })
+  JSON.parse(response.body)
 end
 
-puts profile.get_profile(HEADERS)
+def format_profile(access_token)
+  profile = get_profile(access_token)
+  
+  puts profile["display_name"]
+  uri = profile["uri"]
+  puts uri.split(':').last
+
+end
+
 
 def get_albums(access_token)
   url = "https://api.spotify.com/v1/me/albums"
-
-  response = RestClient.get(url, Authorization: "Bearer #{access_token}")
-
-  return JSON.parse(response.body)
+  response = RestClient.get(url, { Authorization: "Bearer #{access_token}" })
+  JSON.parse(response.body)
 end
 
-puts albums.get_albums(HEADERS)
+def format_albums(access_token)
+  albums_data = get_albums(access_token)
+
+  albums_data["items"].each do |item|
+    album = item["album"]
+    album_name = album["name"]
+    puts "Album: #{album_name}"
+
+    album["artists"].each do |artist|
+      artist_name = artist["name"]
+      puts "  Artist: #{artist_name}"
+    end
+    puts "-" * 40
+  end
+end
+
+access_token = ENV['ACCESS_TOKEN']
+
+puts get_profile(access_token)
+puts format_profile(access_token)
+#albums = get_albums(access_token)
+#teste_album = JSON.pretty_generate(get_albums(access_token))
+#format_albums(access_token)
+puts JSON.pretty_generate(format_albums(access_token))
